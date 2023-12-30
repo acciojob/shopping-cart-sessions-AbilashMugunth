f87d7c95bc627082413c8f6087c3443fd0fff78e
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-30 21:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// This is the boilerplate code given for you
-// You can modify this code
-// Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -11,6 +8,11 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+let cartList = document.querySelector("#cart-list");
+let clearBtn = document.querySelector("#clear-cart-btn");
+
+const storedArray = JSON.parse(sessionStorage.getItem("products"));
+const storedSet = new Set(storedArray);
 
 // Render product list
 function renderProducts() {
@@ -21,11 +23,34 @@ function renderProducts() {
   });
 }
 
+productList.addEventListener("click", (e) => {
+  if (e.target.tagName == "BUTTON") {
+    addToCart(Number(e.target.getAttribute("data-id")));
+  }
+});
+
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  console.log(storedSet);
+  storedSet.forEach((el) => {
+    const product = document.createElement("li");
+    product.textContent = document.querySelector(
+      `[data-id="${el}"]`
+    ).parentElement.childNodes[0].textContent;
+    cartList.appendChild(product);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  Array.from(cartList.children).forEach((pro) => pro.remove());
+
+  storedSet.add(productId);
+
+  sessionStorage.setItem("products", JSON.stringify(Array.from(storedSet)));
+
+  renderCart();
+}
 
 // Remove item from cart
 function removeFromCart(productId) {}
